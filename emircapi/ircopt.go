dc89b6f4ae8e9753742653ab64750a15040c4d41
Alexandru Geana <alex@alegen.net>
2017-12-29 16:45:28 +0100

    adding ircopt.go

---
+++ emircapi/ircopt.go
@@ -0,0 +1,14 @@
+package emircapi
+
+// The IRCOptions interface specifies the options which can be set on an IRCBot implementation. Each method returns a
+// function, which can apply the appropriate configuration to the IRCBot implementation. Each IRCBot implementation
+// needs to also provide a related IRCOptions implementation. Different IRCOptions implementations may not be compatible
+// with the same IRCBot implementation.
+type IRCOptions interface {
+    Nick(nick string)                               func(IRCBot) error;
+    Ident(ident string)                             func(IRCBot) error;
+    Name(name string)                               func(IRCBot) error;
+    Version(version string)                         func(IRCBot) error;
+    Server(address string, port uint, useSSL bool)  func(IRCBot) error;
+    QuitMessage(message string)                     func(IRCBot) error;
+}
