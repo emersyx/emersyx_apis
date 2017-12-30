a4e23a8025e5543c3bab31a0136916812d0ab5d9
Alexandru Geana <alex@alegen.net>
2017-12-30 19:31:12 +0100

    whitespace removed in ircopt.go

--- emircapi/ircopt.go
+++ emircapi/ircopt.go
@@ -3,7 +3,7 @@ package emircapi
 // The IRCOptions interface specifies the options which can be set on an IRCBot implementation. Each method returns a
 // function, which can apply the appropriate configuration to the IRCBot implementation. Each IRCBot implementation
 // needs to also provide a related IRCOptions implementation. The return values of each method of the IRCOptions
-// implementation must be directly usable as arguments to the NewIRCBot implementation.  Different IRCOptions
+// implementation must be directly usable as arguments to the NewIRCBot implementation. Different IRCOptions
 // implementations may not be compatible with the same IRCBot implementation.
 type IRCOptions interface {
     Identifier(id string)                           func(IRCBot) error;
