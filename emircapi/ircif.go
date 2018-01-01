4a07677d905e9d831f729c5e3360446563a03321
Alexandru Geana <alex@alegen.net>
2018-01-01 21:15:09 +0100

    adding documentation

--- emircapi/ircif.go
+++ emircapi/ircif.go
@@ -6,10 +6,21 @@ import(
 
 // The IRCResource interface specifies the methods that must be implemented by any supported IRC resource.
 type IRCResource interface {
+    // This method must start the connection process to the selected IRC server. This must be a blocking method. When
+    // the method returns, the IRCResource must connected to the IRC server if the return value is nil. Otherwise, it is
+    // considered that an error occured and the connection was not possible.
     Connect() error
+    // This method must return a boolean which is true if the bot is connected to the server, and false otherwise.
     IsConnected() bool
+    // This method must disconnect the IRCResource from the IRC server. This must be a blocking method. When the method
+    // returns, the instance must not be connected to the IRC server anymore.
     Quit() error
+    // This method must send a JOIN command to the IRC server. The argument specifies the channel to be joined. If the
+    // instance is not connected to any IRC server, then an error is returned.
     Join(channel string) error
+    // This method must send a PRIVMSG command to the IRC server. The first argument specifies the destination (i.e.
+    // either a user or a channel) and the second argument is the actual message. If the instance is not connected to
+    // any IRC server, then an error is returned.
     Privmsg(destination string, message string) error
 }
 
