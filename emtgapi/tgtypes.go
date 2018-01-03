13d6f66edec6556dabe7f08b404f26e51df9373c
Alexandru Geana <alex@alegen.net>
2018-01-03 13:35:07 +0100

    update tgtypes.go
    add the is_bot field to the User type from the official Telegram Bot API

--- emtgapi/tgtypes.go
+++ emtgapi/tgtypes.go
@@ -20,6 +20,7 @@ type Update struct {
 // https://core.telegram.org/bots/api#user
 type User struct {
     ID                            int64               `json:"id"`
+    IsBot                         bool                `json:"is_bot"`
     FirstName                     string              `json:"first_name"`
     LastName                      string              `json:"last_name"`
     Username                      string              `json:"username"`
