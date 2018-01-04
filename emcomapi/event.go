3c82748c3024deb4a27fd618569cfec5e6fe4875
Alexandru Geana <alex@alegen.net>
2018-01-04 18:45:17 +0100

    fix linter warnings in emcomapi

--- emcomapi/event.go
+++ emcomapi/event.go
@@ -1,8 +1,9 @@
 package emcomapi
 
-// This is the interface for all events supported by the various emersyx components. Event routers should use this type
+// Event is the interface for all events supported by the various emersyx components. Event routers should use this type
 // if they are to support two or more event types. If event routers only support one event type (e.g. emircapi.Message
 // or emtgapi.Update), then they can use that particular type directly.
 type Event interface {
+	// GetSourceIdentifier must return the identifier of the emersyx receptor which generated the event.
     GetSourceIdentifier() string
 }
--- emcomapi/receptor.go
+++ emcomapi/receptor.go
@@ -1,10 +1,10 @@
 package emcomapi
 
-// This is the interface for all event receptors part of the emersyx platform. The emersyx core makes use of this type
-// when loading all receptors. The event routers make use of this type when receiving events from receptors.
+// Receptor is the interface for all event receptors part of the emersyx platform. The emersyx core makes use of this
+// type when loading all receptors. The event routers make use of this type when receiving events from receptors.
 type Receptor interface {
-    // This method must return the identifier of the Receptor instance.
+    // GetIdentifier must return the identifier of the Receptor instance.
     GetIdentifier() string
-    // This method must return the channel via which the Receptor implementation pushes Event objects.
+    // GetEventsChannel must return the channel via which the Receptor implementation pushes Event objects.
     GetEventsChannel() chan Event
 }
