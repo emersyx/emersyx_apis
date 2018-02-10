64b47fb174f05530fbcb851265fda2fc8baa28f0
Alexandru Geana <alex@alegen.net>
2018-02-10 20:40:16 +0100

    add missing import for package io

--- emcomapi/comif.go
+++ emcomapi/comif.go
@@ -1,5 +1,9 @@
 package emcomapi
 
+import (
+	"io"
+)
+
 // Event is the interface for all events supported by the various emersyx components. Event routers should use this type
 // if they are to support two or more event types. If event routers only support one event type (e.g. emircapi.Message
 // or emtgapi.Update), then they can use that particular type directly.
